316d1bf122cec4268a13859d707e5d4aa9820681
samliburd <liburdsam@gmail.com>
2024-07-10 20:41:24 +0100

    combined styles

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 import * as d3 from "d3";
 
+// Load data
 const jsonData = await d3.json("../data/GDP-data.json");
 const data = jsonData.data;
 
+// Define sizes and constants
 const size = {
   width: 875,
   height: 600,
   padding: 50,
   barWidth: null
-}
+};
 
 const tooltipSize = {
   width: 250,
@@ -16,9 +18,11 @@ const tooltipSize = {
   padding: 5,
   offset: 25,
   borderRadius: 5,
-}
+};
 
 size.barWidth = (size.width / data.length);
+
+// Calculate domains
 const calculateDomain = (data) => {
   const dateExtent = d3.extent(data.map((d) => new Date(d[0])));
   const gdpExtent = d3.extent(data.map((d) => d[1]));
@@ -26,48 +30,20 @@ const calculateDomain = (data) => {
   dateExtent[1] = new Date(dateExtent[1].setMonth(dateExtent[1].getMonth() + 3));
 
   return [dateExtent, gdpExtent];
-}
+};
 
 const [dateDomain, gdpDomain] = calculateDomain(data);
 
-const container = d3.select("div")
-  .style("width", `${size.width}px`)
-  .style("height", `${size.height}px`)
-  .style("background-color", "#EEE")
-
-const renderTooltip = (t) => {
-  const { width, height, padding, offset, borderRadius } = t;
-  return d3
-    .select('.container')
-    .append('div')
-    .attr('id', 'tooltip')
-    .style("position", "absolute")
-    .style('opacity', 0)
-    .style("width", `${t.width}px`)
-    .style("height", `${t.height}px`)
-    .style("padding", `${t.padding}px`)
-    .style("border-radius", "5px")
-    .style("background-color", "#CCC")
-
-}
-
-const tooltip = renderTooltip(tooltipSize)
-
-
-
-const xScale = d3
-  .scaleTime()
+// Create scales
+const xScale = d3.scaleTime()
   .domain(dateDomain)
-  .range([size.padding, size.width])
+  .range([size.padding, size.width]);
 
-const yScale = d3
-  .scaleLinear()
+const yScale = d3.scaleLinear()
   .domain(gdpDomain).nice()
-  .range([size.height - size.padding, size.padding])
+  .range([size.height - size.padding, size.padding]);
 
-const newData = data.map((d) => {
-  return [xScale(new Date(d[0])), yScale(d[1])];
-})
+
 
 const invertScale = (d) => {
   const options = {
@@ -76,23 +52,60 @@ const invertScale = (d) => {
     month: 'long',
     day: 'numeric',
   };
-  return [xScale.invert(d[0]).toLocaleString('en-GB', options), Math.round(yScale.invert(d[1]))]
-}
+  return [xScale.invert(d[0]).toLocaleString('en-GB', options), Math.round(yScale.invert(d[1]))];
+};
 
-let currencyFormat = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'})
-console.log(currencyFormat.format("47560"));
-console.log(invertScale(newData[0]));
+let currencyFormat = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
+
+// Create axes
 const xAxis = d3.axisBottom(xScale)
   .ticks(20)
   .tickSizeOuter(0)
-  .tickPadding(5)
-
+  .tickPadding(5);
 
 const yAxis = d3.axisLeft(yScale)
-  .ticks(10)
+  .ticks(10);
 
+// Create container and tooltip
+const container = d3.select("div")
+  .style("width", `${size.width}px`)
+  .style("height", `${size.height}px`)
+  .style("background-color", "#EEE");
 
+const renderTooltip = (t) => {
+  const { width, height, padding, offset, borderRadius } = t;
+  const styles = {
+    position: "absolute",
+    opacity: 0,
+    width: `${t.width}px`,
+    height: `${t.height}px`,
+    padding: `${t.padding}px`,
+    "border-radius": "5px",  // Note: in JavaScript object keys, it's camelCase, not kebab-case
+    "background-color": "#CCC"
+  }
+
+  const tooltip = d3.select('.container')
+    .append('div')
+    .attr('id', 'tooltip')
+    // .style("position", "absolute")
+    // .style('opacity', 0)
+    // .style("width", `${t.width}px`)
+    // .style("height", `${t.height}px`)
+    // .style("padding", `${t.padding}px`)
+    // .style("border-radius", "5px")
+    // .style("background-color", "#CCC");
+  Object.entries(styles).forEach(([prop,val]) => d3.select("#tooltip").style(prop,val))
+
+  return tooltip
+};
+
+// Render SVG and its components
 const renderSVG = () => {
+  const parsedData = data.map((d) => {
+    return [xScale(new Date(d[0])), yScale(d[1])];
+  });
+  const tooltip = renderTooltip(tooltipSize);
+
   const renderAxes = (svg) => {
     svg.append("g")
       .attr("transform", `translate(0, ${size.height - size.padding})`)
@@ -126,7 +139,7 @@ const renderSVG = () => {
 
   const plotBars = (svg) => {
     svg.selectAll("rect")
-      .data(newData)
+      .data(parsedData)
       .enter()
       .append("rect")
       .attr("width", size.barWidth)
@@ -153,5 +166,3 @@ const renderSVG = () => {
 };
 
 renderSVG();
-
-
