71b3d09bf0359010facaaceac802739ca4ec38ac
samliburd <liburdsam@gmail.com>
2024-07-11 13:15:55 +0100

    refactored

--- js/app.js
+++ js/app.js
@@ -1,189 +1,78 @@
 import * as d3 from "d3";
+import { colours } from './colours.js';
 
-// Load data
-const jsonData = await d3.json("../data/GDP-data.json");
-const data = jsonData.data;
-
-// Define sizes and constants
 const size = {
   width: 875,
   height: 600,
   padding: 50,
-  barWidth: null
-};
+  barWidth: 875 / 275
+}
 
-const tooltipSize = {
-  width: 250,
-  height: 50,
-  padding: 5,
-  offset: 25,
-  borderRadius: 5,
-};
-
-size.barWidth = size.width / data.length;
-
-// Utility functions
-const addStyles = (styles, selection) => {
-  Object.entries(styles).forEach(([prop, val]) => selection.style(prop, val));
-};
-
-const addAttrs = (attrs, selection) => {
-  Object.entries(attrs).forEach(([prop, val]) => selection.attr(prop, val));
-};
-
-// Calculate domains
-const calculateDomain = (data) => {
-  const dateExtent = d3.extent(data.map((d) => new Date(d[0])));
-  const gdpExtent = d3.extent(data.map((d) => d[1]));
-  // Adjust the end date by adding 3 months
-  dateExtent[1] = new Date(dateExtent[1].setMonth(dateExtent[1].getMonth() + 3));
-
-  return [dateExtent, gdpExtent];
-};
-
-const [dateDomain, gdpDomain] = calculateDomain(data);
-
-// Function to create scales and invertScale function
-const createScales = (dateDomain, gdpDomain, size) => {
-  const xScale = d3.scaleTime()
-    .domain(dateDomain)
-    .range([size.padding, size.width]);
-
-  const yScale = d3.scaleLinear()
-    .domain(gdpDomain).nice()
-    .range([size.height - size.padding, size.padding]);
-
-  const invertScale = (d) => {
-    const options = {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    };
-    return [xScale.invert(d[0]).toLocaleString('en-GB', options), Math.round(yScale.invert(d[1]))];
-  };
-
-  return { xScale, yScale, invertScale };
-};
-
-const { xScale, yScale, invertScale } = createScales(dateDomain, gdpDomain, size);
-
-let currencyFormat = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
-
-// Create axes
-const xAxis = d3.axisBottom(xScale)
-  .ticks(20)
-  .tickSizeOuter(0)
-  .tickPadding(5);
-
-const yAxis = d3.axisLeft(yScale)
-  .ticks(10);
-
-// Create container and tooltip
-const container = d3.select("div")
-  .style("width", `${size.width}px`)
-  .style("height", `${size.height}px`)
-  .style("background-color", "#EEE");
-
-const renderTooltip = (t) => {
-  const { width, height, padding, offset, borderRadius } = t;
-  const styles = {
-    position: "absolute",
-    opacity: 0,
-    width: `${width}px`,
-    height: `${height}px`,
-    padding: `${padding}px`,
-    "border-radius": "5px",
-    "background-color": "#CCC"
-  };
-
-  const tooltip = d3.select('.container')
-    .append('div')
-    .attr('id', 'tooltip');
-
-  addStyles(styles, tooltip);
-  return tooltip;
-};
-
-// Render SVG and its components
-const renderSVG = () => {
-  const parsedData = data.map((d) => {
-    return [xScale(new Date(d[0])), yScale(d[1])];
-  });
-
-  const tooltip = renderTooltip(tooltipSize);
-
-  const renderAxes = (svg) => {
-    svg.append("g")
-      .attr("transform", `translate(0, ${size.height - size.padding})`)
-      .call(xAxis);
-
-    svg.append("g")
-      .attr("transform", `translate(${size.padding}, ${0})`)
-      .call(yAxis);
-  };
-
-  const handleMouseOver = function (event, d) {
-    const dataValueDate = d3.select(this).attr("data-value-date");
-    const dataValueGdp = d3.select(this).attr("data-value-gdp");
-    const { width, height, padding, offset, borderRadius } = tooltipSize;
-    const pageX = event.pageX;
-    const tooltipX = pageX - width - padding - (size.padding * 2);
-
-    const styles = {
-      opacity: 1,
-      top: `${event.pageY - 100}px`,
-      height: "fit-content"
-    };
-    const attrs = {
-      pageX: pageX,
-      tooltipX: tooltipX
-    };
-
-    tooltip.html(`<strong>Date: </strong>${dataValueDate}<br/><strong>GDP: </strong>${currencyFormat.format(Number(dataValueGdp))}`);
-    addStyles(styles, tooltip);
-    addAttrs(attrs, tooltip);
-
-    if (tooltipX < 10) {
-      tooltip.style("left", `${pageX + size.padding}px`);
-    } else {
-      tooltip.style("left", `${pageX - width - padding - size.padding}px`);
+function applyProperties(properties, selection, type) {
+  Object.entries(properties).forEach(([prop, val]) => {
+    if (type === "style") {
+      selection.style(prop, val);
+    } else if (type === "attr") {
+      selection.attr(prop, val);
     }
-  };
-
-  const handleMouseOut = function () {
-    tooltip.style("opacity", 0);
-  };
-
-  const plotBars = (svg) => {
-    const attrs = {
-      "width": size.barWidth,
-      "height": (d) => size.height - d[1] - size.padding,
-      "x": (d) => d[0],
-      "y": (d) => d[1] - 1,
-      "index": (d, i) => i,
-      "data-value-date": (d) => `${invertScale(d)[0]}`,
-      "data-value-gdp": (d) => `${invertScale(d)[1]}`,
-      "fill": (d, i) => (i % 2 === 0 ? "#CCC" : "#AAA")
-    };
-
-    const bars = svg.selectAll("rect")
-      .data(parsedData)
-      .enter()
-      .append("rect")
-      .on("mouseover", handleMouseOver)
-      .on("mouseout", handleMouseOut);
-
-    addAttrs(attrs, bars);
-  };
-
-  const svg = container
-    .append("svg")
-    .attr("width", size.width + 50)
-    .attr("height", size.height);
-
-  renderAxes(svg);
-  plotBars(svg);
+  });
 };
 
-renderSVG();
+function parseData(data) {
+  const dates = data.map(d => new Date(d[0]));
+  const gdp = data.map(d => d[1]);
+  return {dates, gdp}
+}
+
+function createScales(data, size) {
+  function calculateDomain(data) {
+    return d3.extent(data)
+  }
+
+  const extent = {
+    x: calculateDomain(data.dates),
+    y: calculateDomain(data.gdp)
+  }
+  return {
+    x: d3.scaleTime().domain(extent.x).range([size.padding, size.width]),
+    y: d3.scaleLinear().domain(extent.y).range([size.height - size.padding, size.padding])
+  }
+
+}
+
+function draw(scale) {
+  const container = d3.select("div")
+  const svg = container.append("svg")
+    .attr("width", size.width)
+    .attr("height", size.height)
+  const xAxis = d3.axisBottom(scale.x)
+  const yAxis = d3.axisLeft(scale.y)
+  svg.append("g")
+    .attr("transform", `translate(0, ${size.height - size.padding})`)
+    .call(xAxis)
+    .selectAll("text")
+    .style("fill", colours["Dark Teal"]); // Set axis text colour
+
+  svg.append("g")
+    .attr("transform", `translate(${size.padding}, ${0})`)
+    .call(yAxis)
+    .selectAll("text")
+    .style("fill", colours["Dark Teal"]); // Set axis text colour
+
+}
+
+async function loadData() {
+  // Load data
+  const jsonData = await d3.json("../data/GDP-data.json");
+  return jsonData.data;
+}
+
+
+loadData()
+  .then(data => {
+
+    const scale = createScales(parseData(data), size)
+    // console.log(scale.x(new Date(data[0][0])))
+    draw(scale)
+  })
+
---
+++ js/colours.js
@@ -0,0 +1,14 @@
+// colours.js
+export const colours = {
+  "Yellow": "#f7e64d",
+  "Pink": "#ffc2ff",
+  "White": "#ffffff",
+  "Blue": "#7ee1e8",
+  "Purple": "#6f35a3",
+  "Teal": "#43cfb5",
+  "Light Grey": "#f0f0f0",
+  "Lilac": "#7575df",
+  "Red": "#e8705e",
+  "Dark Teal": "#003643",
+  "Dark Grey": "#616161"
+};
